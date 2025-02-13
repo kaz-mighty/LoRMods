3c1e0c1c3b002b0d9854cda7df5c8ece2d7bc763
kaz_mighty <kaz8032@gmail.com>
2025-02-13 15:41:20 +0900

    add priority script

---
+++ MetaInvitation/Second/DiceCardPriority_kaz_Cooling.cs
@@ -0,0 +1,21 @@
+﻿namespace MetaInvitation.Second
+{
+	class DiceCardPriority_kaz_Cooling : DiceCardPriorityBase
+	{
+		public override int GetPriorityBonus(BattleUnitModel owner)
+		{
+			var maxLight = owner.MaxPlayPoint;
+			var light = owner.PlayPoint - owner.cardSlotDetail.ReservedPlayPoint;
+			int priority = -2;
+			if (maxLight > light)
+			{
+				priority += 1;
+			}
+			if ((owner.bufListDetail.GetActivatedBuf(KeywordBuf.Burn)?.stack ?? 0) >= 3)
+			{
+				priority += 2;
+			}
+			return priority;
+		}
+	}
+}
---
+++ MetaInvitation/Second/DiceCardPriority_kaz_LightTrump.cs
@@ -0,0 +1,20 @@
+﻿namespace MetaInvitation.Second
+{
+	class DiceCardPriority_kaz_LightTrump : DiceCardPriorityBase
+	{
+		public override int GetPriorityBonus(BattleUnitModel owner)
+		{
+			var maxLight = owner.MaxPlayPoint;
+			var light = owner.PlayPoint - owner.cardSlotDetail.ReservedPlayPoint;
+			if (maxLight - light >= 5)
+			{
+				return 2;
+			}
+			else if (maxLight - light >= 2)
+			{
+				return -2;
+			}
+			return -10;
+		}
+	}
+}
