6d288b7ec4c6e8950b8584d85fa20ed4b58f8b84
kaz_mighty <kaz8032@gmail.com>
2025-02-20 22:28:49 +0900

    Change 始末 (Finishing Touch) to 簡易始末, etc...
    Increase HP and Break of KeyPage (Librarian)

---
+++ MetaInvitation/Second/PassiveAbility_kaz_Target25.cs
@@ -0,0 +1,36 @@
+﻿namespace MetaInvitation.Second
+{
+	class PassiveAbility_kaz_Target25 : PassiveAbilityBase
+	{
+		public override void OnUseCard(BattlePlayingCardDataInUnitModel curCard)
+		{
+			if (curCard.target != null && curCard.target.faction != owner.faction)
+			{
+				_nextTarget = curCard.target;
+			}
+		}
+
+		public override void OnRoundEnd()
+		{
+			if (_nextTarget != null && !_nextTarget.IsDead())
+			{
+				foreach (BattleUnitModel battleUnitModel in BattleObjectManager.instance.GetAliveList((owner.faction == Faction.Player) ? Faction.Enemy : Faction.Player))
+				{
+					battleUnitModel.bufListDetail.RemoveBufAll(KeywordBuf.NicolaiTarget);
+				}
+				_nextTarget.bufListDetail.AddBuf(new BattleUnitBuf_Target25());
+			}
+		}
+
+		private BattleUnitModel _nextTarget;
+
+		class BattleUnitBuf_Target25 : PassiveAbility_250223.BattleUnitBuf_target
+		{
+			protected override string keywordId => "kaz_Target25";
+			protected override string keywordIconId => base.keywordId;
+
+			public override int GetDamageIncreaseRate() => 25;
+			public override int GetBreakDamageIncreaseRate() => 25;
+		}
+	}
+}
