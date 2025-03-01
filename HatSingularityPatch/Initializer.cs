6295b375d4e7678d3997e176654d824c5944dc53
kaz_mighty <kaz8032@gmail.com>
2025-03-01 22:18:17 +0900

    Add a patch to replace xml

---
+++ HatSingularityPatch/Initializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+using Mod;
+
+namespace HatPatch
+{
+    public class Initializer : ModInitializer
+    {
+		// This must be called after HatSingularity is loaded and before it is initialized.
+		// Uses the fact that "1FrameworkLoader" loads and initializes files in order of file name.
+		public override void OnInitializeMod()
+		{
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		internal static void AddDisplayLog(string msg, LogType type)
+		{
+			switch (type)
+			{
+				case LogType.Error:
+				case LogType.Assert:
+				case LogType.Exception:
+				default:
+					msg = $"(pid: {packageId}) {msg}";
+					break;
+				case LogType.Warning:
+					msg = $"<color=yellow>(pid: {packageId}) {msg}</color>";
+					break;
+			}
+			Debug.unityLogger.Log(type, msg);
+			(AccessTools.Field(typeof(ModContentManager), "_logs").GetValue(ModContentManager.Instance) as List<string>).Add(msg);
+		}
+
+		public static readonly string packageId = "kazmighty.HatSingularityPatch";
+		public static Harmony harmony = new Harmony(packageId);
+	}
+}
---
+++ HatSingularityPatch/XmlPatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml;
+using System.Reflection;
+using UnityEngine;
+using Mod;
+using HarmonyLib;
+using Hat_Harmony;
+
+namespace HatPatch
+{
+	[HarmonyPatch]
+	class XmlPatch
+	{
+		[HarmonyPatch(typeof(HatInitializer.ExtraLoad), "GetAllResources")]
+		[HarmonyPostfix]
+		internal static void ReplaceResourcesPatch()
+		{
+			ReplaceResources();
+		}
+
+		internal static void ReplaceResources()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			foreach (var resourceName in assembly.GetManifestResourceNames())
+			{
+				var splitName = resourceName.Split('.');
+				var nameBody = splitName[splitName.Count() - 2];
+				var extension = splitName[splitName.Count() - 1];
+				try
+				{
+					switch (extension)
+					{
+						case "xml":
+							var xml = new XmlDocument();
+							xml.Load(assembly.GetManifestResourceStream(resourceName));
+							HatInitializer.Xmls[nameBody] = xml;
+							break;
+					}
+				} 
+				catch (Exception ex)
+				{
+					Debug.LogException(ex);
+					Initializer.AddDisplayLog("Failed to load resource.", LogType.Warning);
+				}
+			}
+		}
+	}
+}
