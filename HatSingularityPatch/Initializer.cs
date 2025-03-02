e700ddfebe40f9844423870f43e5e693ed6253f6
kaz_mighty <kaz8032@gmail.com>
2025-03-03 00:42:45 +0900

    Add version check

--- HatSingularityPatch/Initializer.cs
+++ HatSingularityPatch/Initializer.cs
@@ -7,15 +7,29 @@ using System.Reflection;
 using UnityEngine;
 using HarmonyLib;
 using Mod;
+using Hat_Harmony;
 
 namespace HatPatch
 {
 	public class Initializer : ModInitializer
 	{
 		// This must be called after HatSingularity is loaded and before it is initialized.
-		// Uses the fact that "1FrameworkLoader" loads and initializes files in order of file name.
+		// Uses the fact that "1FrameworkLoader" loads and adds initialization list in order of file name.
 		public override void OnInitializeMod()
 		{
+			var hatVersion = Assembly.GetAssembly(typeof(HatInitializer)).GetName().Version;
+			if (hatVersion != patchTargetVersion)
+			{
+				switch (GlobalGameManager.Instance.CurrentOption.language) {
+					case "jp":
+						AddDisplayLog("HatSingularityのバージョンが1.0.0.9ではありません。パッチがうまくいかない可能性があります。", LogType.Log);
+						break;
+					default:
+						AddDisplayLog("Your HatSingularity version is not 1.0.0.9, so the patch may not work.", LogType.Log);
+						break;
+				}
+			}
+
 			harmony.PatchAll(Assembly.GetExecutingAssembly());
 		}
 
@@ -27,17 +41,23 @@ namespace HatPatch
 				case LogType.Assert:
 				case LogType.Exception:
 				default:
-					msg = $"(pid: {packageId}) {msg}";
+					msg = $"(mod: {packageName}) {msg}";
 					break;
 				case LogType.Warning:
-					msg = $"<color=yellow>(pid: {packageId}) {msg}</color>";
+					msg = $"<color=yellow>(mod: {packageName}) {msg}</color>";
+					break;
+				case LogType.Log:
+					msg = $"<color=green>(mod: {packageName}) {msg}</color>";
 					break;
 			}
 			Debug.unityLogger.Log(type, msg);
 			(AccessTools.Field(typeof(ModContentManager), "_logs").GetValue(ModContentManager.Instance) as List<string>).Add(msg);
 		}
 
+		public static readonly string packageName = "HatSingularityPatch";
 		public static readonly string packageId = "kazmighty.HatSingularityPatch";
 		public static Harmony harmony = new Harmony(packageId);
+
+		public static Version patchTargetVersion = new Version(1, 0, 0, 9);
 	}
 }
