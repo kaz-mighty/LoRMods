5e766673836388020f6a8342c09506b1e0d2b9f9
kaz_mighty <kaz8032@gmail.com>
2025-03-02 23:51:09 +0900

    Fix "Singleton" icon in the filter is not displayed.

---
+++ HatSingularityPatch/OnlyOneFilterPatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using LOR_DiceSystem;
+using HarmonyLib;
+using Hat_Harmony;
+
+namespace HatPatch
+{
+    [HarmonyPatch]
+    class OnlyOneFilterPatch
+    {
+        [HarmonyPatch(typeof(SpecialTextPatch), "TryGetFilterBufName")]
+        [HarmonyPrefix]
+        internal static bool TryGetFilterBufName_Prefix(ref bool __result, string keyword, ref string name)
+        {
+            if (keyword == "OnlyOne_Keyword")
+            {
+                name = BattleEffectTextsXmlList.Instance.GetEffectTextName(keyword);
+                __result = !string.IsNullOrWhiteSpace(name);
+                return false;
+            }
+            return true;
+        }
+
+	}
+}
