208a41e549cdf93330162c6663b6a51e6da82974
kaz_mighty <kaz8032@gmail.com>
2025-03-07 22:50:22 +0900

    Remake multiple preview system

---
+++ HatSingularityPatch/BattleMultiPreviewListUI.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Hat_Harmony;
+using HarmonyLib;
+
+
+namespace HatPatch
+{
+	public class BattleMultiPreviewListUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+	{
+		public BattleDiceCardUI ParentUI { get; private set; }
+		public List<BattleDiceCardModel> PreviewCardList => new List<BattleDiceCardModel>(_previewCardList);
+
+		public List<BattleDiceCardUI> PreviewUiList => new List<BattleDiceCardUI>(_previewUiList);
+
+		public int CurrentPreviewIndex
+		{
+			get => _currentPreviewIndex;
+			set
+			{
+				var beforePreviewIndex = _currentPreviewIndex;
+				_currentPreviewIndex = value;
+				if (_currentPreviewIndex < 0)
+				{
+					_currentPreviewIndex = _previewCardList.Count - 1;
+				}
+				else if (_currentPreviewIndex >= _previewCardList.Count)
+				{
+					_currentPreviewIndex = 0;
+				}
+				if (_currentPreviewIndex != beforePreviewIndex && _isShowDetail)
+				{
+					_previewUiList[beforePreviewIndex].HideDetail();
+					_previewUiList[CurrentPreviewIndex].ShowDetail();
+				}
+			}
+		}
+
+		public bool IsShowDetail
+		{
+			get => _isShowDetail;
+			set
+			{
+				_isShowDetail = value;
+				if (_isShowDetail)
+				{
+					_previewUiList[_currentPreviewIndex].ShowDetail();
+				}
+				else
+				{
+					_previewUiList[_currentPreviewIndex].HideDetail();
+				}
+			}
+		}
+
+		public int BaseSortingOrder { get; set; }
+
+		public void Init(BattleDiceCardUI parentUI)
+		{
+			// To avoid this being copied, do the first UI clone before SetParent.
+			ClonePreviewUI(parentUI);
+
+			var parentVibeRect = (RectTransform)AccessTools.Field(typeof(BattleDiceCardUI), "vibeRect").GetValue(parentUI);
+			transform.SetParent(parentVibeRect, false);
+			ParentUI = parentUI;
+		}
+
+		public void SetPreviewCards(List<BattleDiceCardModel> previewCards)
+		{
+			_previewCardList = previewCards;
+			while (_previewUiList.Count < _previewCardList.Count)
+			{
+				ClonePreviewUI(_previewUiList[0]);
+			}
+
+			for (var i = 0; i < _previewCardList.Count; i++)
+			{
+				var previewUI = _previewUiList[i];
+				previewUI.transform.localPosition = new Vector3(200f - 200f * (_previewCardList.Count * 0.5f - i), 1320f, 0f);
+				previewUI.gameObject.SetActive(true);
+				previewUI.SetCard(_previewCardList[i]);
+				previewUI.SetDefault();
+				previewUI.ResetSiblingIndex();
+			}
+			for (var i = _previewCardList.Count; i < _previewUiList.Count; i++)
+			{
+				_previewUiList[i].gameObject.SetActive(false);
+			}
+			CurrentPreviewIndex = 0;
+		}
+
+		public void ShowPreview()
+		{
+			gameObject.SetActive(true);
+		}
+
+		public void HidePreview()
+		{
+			IsShowDetail = false;
+			gameObject.SetActive(false);
+		}
+
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			IsShowDetail = true;
+		}
+
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			IsShowDetail = false;
+		}
+
+		public void Update()
+		{
+			if (Input.GetKeyUp(KeyCode.Tab))
+			{
+				HidePreview();
+				return;
+			}
+			if (!IsShowDetail)
+			{
+				return;
+			}
+			BattleCamManager.Instance.scrollable = false;
+			if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetAxis("Mouse ScrollWheel") <= -0.1f)
+			{
+				CurrentPreviewIndex += 1;
+			}
+			if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetAxis("Mouse ScrollWheel") >= 0.1f)
+			{
+				CurrentPreviewIndex -= 1;
+			}
+		}
+
+		private void ClonePreviewUI(BattleDiceCardUI from)
+		{
+			// Awake may not be called
+			var childPreviewUI = Instantiate(from, transform);
+
+			AccessTools.Method(typeof(CardPreviewPatch), "DisableZoom").Invoke(null, new object[] { childPreviewUI });
+			childPreviewUI.name = ExtraPreviewReplace.HatObjectName + "_" + _previewUiList.Count;
+			childPreviewUI.scaleOrigin = Vector3.one * 0.85f;
+			childPreviewUI.transform.localScale = childPreviewUI.scaleOrigin;
+			childPreviewUI.transform.localRotation = Quaternion.Euler(0f, 0f, -1f);
+
+			/* insert SiblingIndex */
+			var defaultIdxInfo = AccessTools.Field(typeof(BattleDiceCardUI), "_defaultIdx");
+			childPreviewUI.transform.SetSiblingIndex(0);
+			defaultIdxInfo.SetValue(childPreviewUI, 0);
+			foreach (var previewUi in _previewUiList)
+			{
+				defaultIdxInfo.SetValue(previewUi, (int)defaultIdxInfo.GetValue(previewUi) + 1);
+			}
+
+			_previewUiList.Add(childPreviewUI);
+		}
+
+		bool _isShowDetail = false;
+		int _currentPreviewIndex;
+		List<BattleDiceCardUI> _previewUiList = new List<BattleDiceCardUI>();
+		List<BattleDiceCardModel> _previewCardList;
+
+	}
+}
--- HatSingularityPatch/ExtraPreviewPatch.cs
+++ HatSingularityPatch/ExtraPreviewPatch.cs
@@ -4,8 +4,6 @@ using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 using HarmonyLib;
-using Hat_Effect;
-using Hat_Harmony;
 
 
 namespace HatPatch
@@ -30,7 +28,7 @@ namespace HatPatch
 					break;
 				case "OnClick":
 				case "ShowDetail":
-					insertMethod = AccessTools.Method(typeof(ExtraPreviewPatch), "SetOrder");
+					insertMethod = AccessTools.Method(typeof(ExtraPreviewPatch), "SetAsLastOrder");
 					break;
 				default:
 					throw new ArgumentException("The patch attribute has an invalid value.");
@@ -50,12 +48,18 @@ namespace HatPatch
 			yield break;
 		}
 
-		static void ResetOrder(BattleDiceCardUI instance)
+		internal static void ResetOrder(BattleDiceCardUI instance)
 		{
-			var canvas = instance.gameObject.GetComponent<Canvas>();
-			if (canvas != null)
+			if (instance.gameObject.TryGetComponent<Canvas>(out var canvas))
 			{
-				canvas.sortingOrder = instance.transform.GetSiblingIndex() * 2 + 1351;
+				if (instance.transform.parent.TryGetComponent<BattleMultiPreviewListUI>(out var multiUI))
+				{
+					canvas.sortingOrder = instance.transform.GetSiblingIndex() * 2 + multiUI.BaseSortingOrder;
+				}
+				else
+				{
+					canvas.sortingOrder = instance.transform.GetSiblingIndex() * 2 + BaseSortingOrder;
+				}
 
 				// Also fix the Renderer Order of the Book Hunter Mod
 				foreach (var renderer in instance.gameObject.GetComponentsInChildren<Renderer>())
@@ -65,12 +69,18 @@ namespace HatPatch
 			}
 		}
 
-		static void SetOrder(BattleDiceCardUI instance)
+		internal static void SetAsLastOrder(BattleDiceCardUI instance)
 		{
-			var canvas = instance.gameObject.GetComponent<Canvas>();
-			if (canvas != null)
+			if (instance.gameObject.TryGetComponent<Canvas>(out var canvas))
 			{
-				canvas.sortingOrder = instance.transform.parent.childCount * 2 + 1351;
+				if (instance.transform.parent.TryGetComponent<BattleMultiPreviewListUI>(out var multiUI))
+				{
+					canvas.sortingOrder = instance.transform.parent.childCount * 2 + multiUI.BaseSortingOrder;
+				}
+				else
+				{
+					canvas.sortingOrder = instance.transform.parent.childCount * 2 + BaseSortingOrder;
+				}
 
 				// Also fix the Renderer Order of the Book Hunter Mod
 				foreach (var renderer in instance.gameObject.GetComponentsInChildren<Renderer>())
@@ -80,170 +90,7 @@ namespace HatPatch
 			}
 		}
 
-		[HarmonyPatch(typeof(UI_BattleExtraCardPreview), "Reset")]
-		[HarmonyPatch(typeof(UI_BattleExtraCardPreview), "Update")]
-		[HarmonyTranspiler]
-		static IEnumerable<CodeInstruction> FixSetOrder(IEnumerable<CodeInstruction> codes, MethodBase original)
-		{
-			// Modify the order after substituting into SortingOrder.
-			// (Replacement is difficult, so only insert)
-			var targetMethod = AccessTools.Method(typeof(Canvas), "set_sortingOrder");
-			var isFirst = true;
-			int lastLocalUI = -1;
-			var localInfo = original.GetMethodBody().LocalVariables;
-			foreach (var code in codes)
-			{
-				yield return code;
-
-				// Get local variables to support changes to build configurations
-				if (code.IsLdloc())
-				{
-					var index = code.LocalIndex();
-					if (index < localInfo.Count && localInfo[index].LocalType == typeof(BattleDiceCardUI))
-					{
-						lastLocalUI = index;
-					}
-				}
-				if (code.Calls(targetMethod))
-				{
-					if (lastLocalUI == -1)
-					{
-						Debug.Log($"(Mod: {Initializer.packageName}) Patch ExtraPreviewPatch.FixSetOrder failed.");
-						continue;
-					}
-					if (isFirst)
-					{
-						// diff:
-						//   battleDiceCardUI.transform.gameObject.GetComponent<Canvas>().sortingOrder = /* any */
-						// + battleDiceCardUI.ResetSiblingIndex();
-						// + ResetOrder(battleDiceCardUI);
-						yield return new CodeInstruction(OpCodes.Ldloc, lastLocalUI);
-						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(BattleDiceCardUI), "ResetSiblingIndex"));
-						yield return new CodeInstruction(OpCodes.Ldloc, lastLocalUI);
-						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraPreviewPatch), "ResetOrder"));
-						isFirst = false;
-					}
-					else
-					{
-						// diff:
-						//   battleDiceCardUI.transform.gameObject.GetComponent<Canvas>().sortingOrder = /* any */
-						// + SetMaxSiblingIndex(battleDiceCardUI);
-						// + SetOrder(battleDiceCardUI);
-						yield return new CodeInstruction(OpCodes.Ldloc, lastLocalUI);
-						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraPreviewPatch), "SetMaxSiblingIndex"));
-						yield return new CodeInstruction(OpCodes.Ldloc, lastLocalUI);
-						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraPreviewPatch), "SetOrder"));
-					}
-				}
-			}
-
-			yield break;
-		}
-
-		static void SetMaxSiblingIndex(BattleDiceCardUI __instance)
-		{
-			__instance.transform.SetSiblingIndex(__instance.transform.parent.childCount - 1);
-		}
-
-		[HarmonyPatch(typeof(CardPreviewPatch), "BattleDiceCardUI_ShowDetail_Post")]
-		[HarmonyTranspiler]
-		static IEnumerable<CodeInstruction> FixShowDetailPatch(IEnumerable<CodeInstruction> codes, ILGenerator ilGen)
-		{
-			// insert at top:
-			// if (IsCardSelected()) { return; }
-			var skipLabel = ilGen.DefineLabel();
-			var nop = new CodeInstruction(OpCodes.Nop);
-			nop.labels.Add(skipLabel);
-			yield return CodeInstruction.Call(typeof(ExtraPreviewPatch), "IsCardSelected");
-			yield return new CodeInstruction(OpCodes.Brfalse_S, skipLabel);
-			yield return new CodeInstruction(OpCodes.Ret);
-			yield return nop;
-
-			var targetMethod = AccessTools.Method(typeof(CardPreviewPatch), "DisableZoom");
-			foreach (var code in codes)
-			{
-				if (code.Calls(targetMethod))
-				{
-					// diff:
-					// + ResetDefaultIndex(battleDiceCardUI);
-					//   CardPreviewPatch.DisableZoom(battleDiceCardUI);
-					yield return new CodeInstruction(OpCodes.Dup);
-					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraPreviewPatch), "ResetDefaultIndex"));
-
-				}
-				yield return code;
-			}
-
-			yield break;
-		}
-
-		static bool IsCardSelected()
-		{
-			return BattleManagerUI.Instance.ui_unitCardsInHand.IsCardSelected();
-		}
-
-		static void ResetDefaultIndex(BattleDiceCardUI instantiateUI)
-		{
-			var defaultIdxInfo = AccessTools.Field(typeof(BattleDiceCardUI), "_defaultIdx");
-
-			instantiateUI.transform.SetSiblingIndex(0);
-			defaultIdxInfo.SetValue(instantiateUI, 0);
-
-			foreach (Transform child in instantiateUI.transform.parent)
-			{
-				if (child.gameObject.name.Contains("Hat_BattleExtraPreviewCard"))
-				{
-					var childUI = child.GetComponent<BattleDiceCardUI>();
-					defaultIdxInfo.SetValue(childUI, (int)defaultIdxInfo.GetValue(childUI) + 1);
-				}
-			}
-		}
-
-		[HarmonyPatch(typeof(UI_BattleExtraCardPreview), "ShowDetails")]
-		[HarmonyPostfix]
-		static void ShowDetails_Patch(UI_BattleExtraCardPreview __instance)
-		{
-			__instance.enabled = true;
-		}
-
-		[HarmonyPatch(typeof(UI_BattleExtraCardPreview), "HideDetails")]
-		[HarmonyPostfix]
-		static void HideDetails_Patch(UI_BattleExtraCardPreview __instance)
-		{
-			__instance.enabled = false;
-		}
-
-		[HarmonyPatch(typeof(BattleDiceCardUI), "HideDetail")]
-		[HarmonyPostfix]
-		static void CancelExtraDetail(BattleDiceCardUI __instance)
-		{
-			if (__instance.gameObject.name.Contains("Hat_BattleExtraPreviewCard")) {
-				AccessTools.Field(typeof(CardPreviewPatch), "isPreviewBattleCard").SetValue(null, false);
-			}
-		}
-
-		[HarmonyPatch(typeof(BattleDiceCardUI), "OnPdSubmit")]
-		[HarmonyPrefix]
-		static bool CancelClick(BattleDiceCardUI __instance)
-		{
-			// The flashing when vibrating issue causes further problems,
-			// so it would be simpler to just disable the click altogether.
-			if (__instance.gameObject.name.Contains("Hat_BattleExtraPreviewCard"))
-			{
-				return false;
-			}
-			return true;
-		}
-
-		[HarmonyPatch(typeof(BattleDiceCardUI), "SetDefault")]
-		[HarmonyPostfix]
-		static void SetDefaultExtra(BattleDiceCardUI __instance)
-		{
-			var component = __instance.gameObject.GetComponent<UI_BattleExtraCardPreview>();
-			if (component != null)
-			{
-				component.HideDetails();
-			}
-		}
+		// BattleUnitCardsInHandUI Canvas Order == 1350
+		internal const int BaseSortingOrder = 1350 + 1;
 	}
 }
