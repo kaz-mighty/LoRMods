6f5208825fa487dbacadacf0ceb93d2de1c521f2
kaz_mighty <kaz8032@gmail.com>
2025-03-14 19:30:10 +0900

    Fix mod conflicts

---
+++ SimpleModListSorter/PatchClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using LOR_DiceSystem;
+using HarmonyLib;
+using Mod;
+
+namespace SimpleModListSorter
+{
+	[HarmonyPatch]
+	class PatchClass
+	{
+		[HarmonyPatch(typeof(ModContentManager), "SaveSelectionData")]
+		[HarmonyPrefix]
+		static void SaveSelectionDataPrefix()
+		{
+			Initializer.OnAllModInitialized();
+		}
+	}
+}
